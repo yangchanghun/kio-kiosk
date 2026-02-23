dc8e95f24691ff99fc7dd55ddb370c910e6ec1bc
yangchanghun <gory4848@naver.com>
2026-02-23 09:21:57 +0900

    pwa

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,9 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true, // 개발 모드에서도 PWA 테스트 가능
+      },
+      manifest: {
+        name: "간단키오에듀",
+        short_name: "간단키오에듀",
+        description: "My React + Vite PWA",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/rv512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
