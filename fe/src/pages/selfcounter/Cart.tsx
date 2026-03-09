205d96a05d798500fbd69ab99a5301bba84e2440
yangchanghun <gory4848@naver.com>
2026-03-09 17:23:05 +0900

    셀프계산대

--- fe/src/pages/selfcounter/Cart.tsx
+++ fe/src/pages/selfcounter/Cart.tsx
@@ -20,16 +20,34 @@ export default function Cart() {
   return (
     <div className="min-h-screen bg-gray-200 flex justify-center">
       {/* 모바일은 w-full, md 이상은 720px */}
+
       <div className="w-full md:w-[720px] min-h-screen bg-[#A8CBB3] flex flex-col shadow-xl">
         {/* 상단 이미지 */}
-        <div className="w-full">
+
+        <div className="w-full relative">
+          {" "}
+          {/* relative 추가 */}
+          {/* 이전 버튼 (왼쪽 상단 배치) */}
+          <button
+            onClick={() => navigate("/")}
+            className="
+              absolute top-4 left-4 z-10
+              bg-white/80 hover:bg-white 
+              rounded-full p-2 
+              shadow-md
+              transition-transform active:scale-95
+               focus:outline-none
+            "
+            style={{ WebkitTapHighlightColor: "transparent" }}
+          >
+            {/* back.svg 크기를 적절히 조절했습니다 (w-12 h-12) */}
+            <img src="/back.svg" className="w-12 h-12" alt="뒤로가기" />
+          </button>
+          {/* 배너 이미지 */}
           <img
-            onClick={() => {
-              navigate("/itemadd");
-            }}
             src="/kio-banner.png"
             alt="상단 타이틀"
-            className="w-full object-cover"
+            className="w-full object-cover cursor-pointer"
           />
         </div>
 
